a27b1dc3ceaa170b603095642483080054ffbcfc
danielpinon <danielpinon@danielpinon.com.br>
2022-08-28 10:46:48 -0300

    Feat: Ajustes das cores e imagens do sistema

--- resources/views/layouts/footers/auth.blade.php
+++ resources/views/layouts/footers/auth.blade.php
@@ -1,11 +1,11 @@
 <footer class="footer">
   <div class="container-fluid">
-    <div class="copyright float-right">
+    {{-- <div class="copyright float-right">
       &copy;
       <a href="https://agildesenvolvimento.com" target="_blank">Ágil Devs</a>
       <script>
         document.write(new Date().getFullYear())
       </script>.
-    </div>
+    </div> --}}
   </div>
 </footer>
--- resources/views/layouts/page_templates/auth.blade.php
+++ resources/views/layouts/page_templates/auth.blade.php
@@ -1,6 +1,6 @@
 <div class="wrapper ">
   @include('layouts.navbars.sidebar')
-  <div class="main-panel">
+  <div class="main-panel" style="background-image: url('{{ asset('material') }}/img/backgroud.jpg'); background-size: cover; background-position: top center;align-items: center;">
     @include('layouts.navbars.navs.auth')
     @yield('content')
     @include('layouts.footers.auth')
